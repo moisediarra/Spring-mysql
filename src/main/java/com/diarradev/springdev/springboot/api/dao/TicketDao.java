b0ef2a792055863e992d60f1fa84f4ee738854af
Moise DIARRA <moisepr@gmail.com>
2022-02-21 12:11:46 +0000

    Update Interface TicketDao

--- src/main/java/com/diarradev/springdev/springboot/api/dao/TicketDao.java
+++ src/main/java/com/diarradev/springdev/springboot/api/dao/TicketDao.java
@@ -1,4 +1,8 @@
 package com.diarradev.springdev.springboot.api.dao;
 
-public interface TicketDao {
+import com.diarradev.springdev.springboot.api.model.Ticket;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TicketDao extends CrudRepository<Ticket,Integer>{
+
 }
