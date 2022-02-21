b2c4b0f7ff3bfd71301509f5dc188d67f3e9b080
Moise DIARRA <moisepr@gmail.com>
2022-02-21 11:55:03 +0000

    Update Ticket file

--- src/main/java/com/diarradev/springdev/springboot/api/model/Ticket.java
+++ src/main/java/com/diarradev/springdev/springboot/api/model/Ticket.java
@@ -1,4 +1,17 @@
 package com.diarradev.springdev.springboot.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Ticket")
 public class Ticket {
+    @Id
+    @GeneratedValue
+    private int id;
+    private double amount;
+    private String category;
 }
