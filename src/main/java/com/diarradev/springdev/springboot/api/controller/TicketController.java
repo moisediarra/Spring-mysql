89b981ce2d97a41e112ee27f00c79d0e30681a9b
Moise DIARRA <moisepr@gmail.com>
2022-02-21 12:31:45 +0000

    Update Controller file TicketController

--- src/main/java/com/diarradev/springdev/springboot/api/controller/TicketController.java
+++ src/main/java/com/diarradev/springdev/springboot/api/controller/TicketController.java
@@ -1,4 +1,30 @@
 package com.diarradev.springdev.springboot.api.controller;
+import com.diarradev.springdev.springboot.api.dao.TicketDao;
+import com.diarradev.springdev.springboot.api.model.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/ticket")
 public class TicketController {
+    @Autowired
+    private TicketDao dao;
+
+    @PostMapping("/bookTickets")
+    public String bookTicket(List<Ticket> tickets){
+        dao.save(tickets);
+        return "tickets booked : " + tickets.size();
+    }
+
+    @GetMapping("/getTickets")
+    public List<Ticket> getTickets(){
+        return (List<Ticket>) dao.findAll();
+
+    }
+
 }
